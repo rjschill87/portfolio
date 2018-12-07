cbbc5568d5724539bc0e91c74afd760ec5812f27
Robert Schillinger <schillinger.rob@gmail.com>
2018-12-07 13:01:11 -0600

    Init commit

---
+++ pages/contact.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <div
+        v-for="(contact, i) of contacts"
+        :key="i"
+        class="column">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title has-text-grey">
+              {{ contact.title }}
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content has-text-centered">
+              <b-icon
+                :icon="contact.icon"
+                size="is-large"
+                type="is-primary"/>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <div
+              class="card-footer-item"
+              v-html="contact.content"/>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      contacts: [
+        { icon: 'email', title: 'Email', content: `<a href="mailto:schillinger.rob@gmail.com">Send me an email.</a>`},
+        { icon: 'github-circle', title: 'Github', content: `<a href="https://github.com/rjschill87">Find me on Github.</a>`},
+        { icon: 'linkedin-box', title: 'LinkedIn', content: `<a href="https://www.linkedin.com/in/rjschill/">Message me on LinkedIn.</a>`}
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          {{ title }}
+        </h1>
+      </div>
+      <div class="container">
+        <div class="content is-medium">
+          <p>{{ content }}</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      title: 'About',
+      content: `My name is Rob, and I'm a full-stack developer. I work with a number of technologies, including but not limited to PHP, Node.js, React, React Native, MongoDB and Sass. I always appreciate a good challenge, so let me know if you want to build something together!`
+    }
+  }
+}
+</script>
+
+<style>
+  .content {
+    padding: 1em 5em 0 0;
+  }
+</style>
